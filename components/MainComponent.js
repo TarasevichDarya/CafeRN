f8fa169560c9a75bb43b43ecfe7f7ee94581aefb
Darya Tarasevich <d.tarasevich@softteco.com>
2020-08-12 13:51:57 +0300

    Components, part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import DishDetail from './DishdetailComponent';
+import { SafeAreaView } from 'react-native';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            selectedDish: null
         }
+    }
+
+    onDishSelect(dishId) {
+        this.setState({selectedDish: dishId});
     }
 
     render() {
         return(
-            <Menu dishes={this.state.dishes} />
+            <SafeAreaView>
+                <Menu 
+                    dishes={this.state.dishes} 
+                    onPress={(dishId) => this.onDishSelect(dishId)}
+                    />
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            </SafeAreaView>
         );
     }
 }
